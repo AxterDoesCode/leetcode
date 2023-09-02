405499ab2c9d0aa831437844c11e018a48ed8795
AxterDoesCode <chau.alex88@hotmail.com>
2023-09-02 20:13:33 +0100

    Sliding window technique hmmm

--- 3-Longest Substring Without Repeating Characters.go
+++ 3-Longest Substring Without Repeating Characters.go
@@ -1,7 +1,11 @@
 package main
 
-import "math"
+import (
+	"math"
+	"strings"
+)
 
+//This solution uses sliding window techinique
 func lengthOfLongestSubstring(s string) int {
     longest := 0
     start := 0
@@ -9,6 +13,7 @@ func lengthOfLongestSubstring(s string) int {
     n := len(s)
     for end := 0; end < n; end++ {
         duplicateIndex, isDuplicate := charIndexMap[s[end]]
+        //If it is a duplicate calculate
         if isDuplicate {
             longest = int(math.Max(float64(longest), float64(end - start)))
             for i := start; i <= duplicateIndex; i++ {
@@ -21,4 +26,22 @@ func lengthOfLongestSubstring(s string) int {
     longest = int(math.Max(float64(longest), float64(n-start)))
     return longest
 
+}
+//This one is crazy
+//It is sliding window but it uses the strings std lib to do the checking
+func lengthOfLongestSubstring2(s string) int {
+    var highest, distance int
+    prevPos := -1
+    for i, v := range s {
+        c := string(v)
+        pos := strings.LastIndex(s[:i], c)
+        if pos > prevPos {
+            prevPos = pos
+        }
+        distance = i - prevPos
+        if distance > highest {
+            highest = distance
+        }
+    }
+    return highest
 }
